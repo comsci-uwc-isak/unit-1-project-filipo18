dc6f1c89505bd177a0bec0ab263943d95c7857ec
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-01 21:57:29 +0900

    Add files via upload

---
+++ record.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#This program will recor car trip and
+#write data into plate.txt file
+
+#changing arguments into variables
+plate=$1
+km=$2
+dateout=$3
+datein=$4
+
+#checking if file plate.txt exist, and if user eneterd
+#4 arguments
+
+if [[ ($# -ne 4) ]]; then
+  echo "Incorrect input. Please enter Plate, kilometers, dateout, datein Exiting the program... "
+  exit
+
+elif [ ! -f "$1.txt" ]; then
+  echo "Car don't exist, please crate a car, exiting the program... "
+
+#creating plate.txt file with data about each specific car
+else
+  echo "$plate $km $dateout $datein" > ~/Desktop/RentalCarApp/db/$Plate.txt
+fi
