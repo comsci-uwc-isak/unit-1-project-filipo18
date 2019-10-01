fd7aae18eb5eab9b1f09e243cec8400107026a2b
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-01 22:03:03 +0900

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#This program creates new car based on arguments entered by user
+#it will crate maincar file and record file for the car
+
+
+
+#checking if user enetered 4 arguments, ad exiting the program if not
+if [[ ($# -ne 4) ]]; then
+  echo "Incorrect input. Please enter Plate, Model, Color, Passengers. Exiting the program... "
+  exit
+fi
+
+
+#All data is entered, program will continue
+plate=$1
+model=$2
+color=$3
+pp=$4
+
+#adding new entry to file maincarfile.txt
+echo "$plate $model $color $pp" >> ~/Desktop/RentalCarApp/db/maincarfile.txt
+echo "" > ~/Desktop/RentalCarApp/db/$plate.txt 
+bash frame.sh "Car created successfully"
