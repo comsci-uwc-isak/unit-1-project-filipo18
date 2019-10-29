abf35a5ca659752f5d6078f41b73a8f1bd9a579a
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-29 16:54:21 +0900

    Update edit.sh

--- scripts/edit.sh
+++ scripts/edit.sh
@@ -3,19 +3,23 @@
 #maincarfile
 #user enters [license place] [model] [red] [pp]
 
+#checking if user eneterd correct number of arguments
 if [ $# -ne 4 ]; then
   echo "Error with the number of arguments"
-  echo "Enter License Maker Model Passengers"
+  echo "Enter License Model Color Passengers"
   exit
 fi
 
+#Making arguments variables
 license=$1
 maker=$2
 model=$3
 pp=$4
 
+#Changing to right directory
 cd ../Database
 
+#Checking if wanted car exists
 if [ ! -f "$license.txt" ]; then
   echo "File not found!"
 fi
