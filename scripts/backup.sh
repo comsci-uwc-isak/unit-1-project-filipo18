400b2124a95e18fdfa100ef95e2e24194df48c0a
filipo18 <54606994+filipo18@users.noreply.github.com>
2019-10-29 16:57:04 +0900

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+#This program will backup the entirity of the database folder within the MinimalCarRental application.
+
+location=$1
+
+if [[ $# -ne 1 ]]; then
+	echo "Sorry, there was an error with backing up your files."
+	exit
+else
+	cp -a /Users/rikiodahlgren/Desktop/RentalCarApp/db $location
+	echo "Successfully backed up into $location."
+fi
